f02b464276164f8ce43d747f82d8116b1e1070a9
matteo.merli <matteo.merli@09a6797d-2d1c-0410-8366-433173d232cd>
2006-08-30 20:34:38 +0000

    Documentation changes.

--- trunk/examples/example1.cpp
+++ trunk/examples/example1.cpp
@@ -3,20 +3,30 @@
 
 #include <mm/cache_map.hpp>
 
-static const int S =  80000;
+const int Size =  80000;
 
 typedef mm::cache_map<std::string,double> Map;
 
 int main ( int argc, char * const argv[] ) 
 {
-    Map map( S );
+    // Create the map with the specified size and set the empty key
+    // value (this is actually the empty string).
+    Map map( Size );
     map.set_empty_key( "" );
 
+    // Insert an item in the map. Map::value_type is an alias to, in
+    // this case, std::pair<std::string,double>.
     map.insert( Map::value_type( "ciao", 11.0 ) );    
     
+    // Like std::map, this is the alternate form for inserting items
+    // in the map.
     map[ "prova" ] = 1.5;
     map[ "hola" ] = 3.1;
     map[ "2" ] = 2.2;
+
+    // Be careful that, as in std::map, if you look for a key that
+    // doesn't exists, it will be inserted with default value. 
+    double d = map[ "not existant key" ];
 
     for ( Map::iterator it = map.begin(); it != map.end(); ++it  )
     {
