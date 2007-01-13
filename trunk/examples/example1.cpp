a3e37b24a43f60c679046359c0ce2ca55f110778
matteo.merli <matteo.merli@09a6797d-2d1c-0410-8366-433173d232cd>
2007-01-13 15:11:30 +0000

    Added documentation on public methods.

--- trunk/examples/example1.cpp
+++ trunk/examples/example1.cpp
@@ -3,16 +3,12 @@
 
 #include <mm/cache_map.hpp>
 
-const int Size =  80000;
-
 typedef mm::cache_map<std::string,double> Map;
 
 int main ( int argc, char * const argv[] ) 
 {
-    // Create the map with the specified size and set the empty key
-    // value (this is actually the empty string).
-    Map map( Size );
-    map.set_empty_key( "" );
+    // Create the map.
+    Map map;
 
     // Insert an item in the map. Map::value_type is an alias to, in
     // this case, std::pair<std::string,double>.
@@ -25,8 +21,16 @@ int main ( int argc, char * const argv[] )
     map[ "2" ] = 2.2;
 
     // Be careful that, as in std::map, if you look for a key that
-    // doesn't exists, it will be inserted with default value. 
-    double d = map[ "not existant key" ];
+    // doesn't exists, it will be inserted with default value.
+    // To avoid this behaviour, use the insert() method to add item,
+    // and find() to retrieve them.
+    std::cout << "Printing a not existing item: "
+              << map[ "not existant key" ] << std::endl;
+
+    Map::iterator it = map.find( "test_key" );
+    std::cout << "Looking for 'test_key': "
+              << ( it == map.end() ? "not found" : "found" )
+              << std::endl;
 
     for ( Map::iterator it = map.begin(); it != map.end(); ++it  )
     {
@@ -36,4 +40,7 @@ int main ( int argc, char * const argv[] )
 
     std::cout << "Items: " << map.size() << std::endl;
     std::cout << "Key collisions: " << map.num_collisions() << std::endl;
+
+    std::cout << std::endl
+              << "Map empty key: '" << map.get_empty_key() << "'" << std::endl;
 }
--- trunk/mm/hash_fun.hpp
+++ trunk/mm/hash_fun.hpp
@@ -38,42 +38,111 @@
 #define _MM_HASH_FUN_HPP__
 
 #include <cstddef>
+#include <cstring>
+#include <string>
 
 namespace mm
 {
 
 using std::size_t;
 
-template <class T> inline void hash_combine( size_t& seed, const T& v );
-
-
 ///////////////////////////////////////////////////////////////////////
 // Scalar integers
 ///////////////////////////////////////////////////////////////////////
 
-inline size_t hash_value( char n )           { return static_cast<size_t>( n ); }
-inline size_t hash_value( unsigned char n )  { return static_cast<size_t>( n ); }
-inline size_t hash_value( short n )          { return static_cast<size_t>( n ); }
-inline size_t hash_value( unsigned short n ) { return static_cast<size_t>( n ); }
-inline size_t hash_value( int n )            { return static_cast<size_t>( n ); }
-inline size_t hash_value( unsigned int n )   { return static_cast<size_t>( n ); }
-inline size_t hash_value( long n )           { return static_cast<size_t>( n ); }
-inline size_t hash_value( unsigned long n )  { return static_cast<size_t>( n ); }
+/** Hash value specialization for @a char
+ *  @return the hash value
+ *  @relates hash
+ */
+inline size_t hash_value( char n )
+{ return static_cast<size_t>( n ); }
 
+/** Hash value specialization for @a unsigned @char
+ *  @return the hash value
+ *  @relates hash
+ */
+inline size_t hash_value( unsigned char n )
+{ return static_cast<size_t>( n ); }
+
+/** Hash value specialization for @a short
+ *  @return the hash value
+ *  @relates hash
+ */
+inline size_t hash_value( short n )
+{ return static_cast<size_t>( n ); }
+
+/** Hash value specialization for @a unsigned @char
+ *  @return the hash value
+ *  @relates hash
+ */
+inline size_t hash_value( unsigned short n )
+{ return static_cast<size_t>( n ); }
+
+/** Hash value specialization for @a int
+ *  @return the hash value
+ *  @relates hash
+ */
+inline size_t hash_value( int n )
+{ return static_cast<size_t>( n ); }
+
+/** Hash value specialization for @a unsigned @int
+ *  @return the hash value
+ *  @relates hash
+ */
+inline size_t hash_value( unsigned int n )
+{ return static_cast<size_t>( n ); }
+
+/** Hash value specialization for @a long
+ *  @return the hash value
+ *  @relates hash
+ */
+inline size_t hash_value( long n )
+{ return static_cast<size_t>( n ); }
+
+/** Hash value specialization for @a unsigned long
+ *  @return the hash value
+ *  @relates hash
+ */
+inline size_t hash_value( unsigned long n )
+{ return static_cast<size_t>( n ); }
+    
 ///////////////////////////////////////////////////////////////////////
 // Sequences
 ///////////////////////////////////////////////////////////////////////
 
+/** Hash value specialization for a range of elements.
+ *
+ *  Compute the combined hash value for a collection of items.
+ *
+ *  @param first iterator that points to the first item in the range
+ *               (included)
+ *  @param last iterator that points to the last item in the range
+ *               (excluded)
+ *  @return the hash value
+ *  @relates hash
+ */
 template <class Iterator>
 inline size_t hash_range( Iterator first, Iterator last )
 {
     size_t h = 0;
     for ( ; first != last; ++first )
         hash_combine( h, *first );
-
+    
     return h;
 }
 
+/** Hash value specialization for a range of elements.
+ *
+ *  Compute the combined hash value for a collection of items.
+ *
+ *  @param seed the intial seed value. it is also used as an output
+ *              parameter, as it will contains the combined hash
+ *  @param first iterator that points to the first item in the range
+ *               (included)
+ *  @param last iterator that points to the last item in the range
+ *               (excluded)
+ *  @relates hash
+ */
 template <class Iterator>
 inline void hash_range( size_t& seed, Iterator first, Iterator last )
 {
@@ -81,54 +150,95 @@ inline void hash_range( size_t& seed, Iterator first, Iterator last )
         hash_combine( seed, *first );
 }
 
+/** Hash value specialization for @a char*
+ *  @return the hash value
+ *  @relates hash
+ */
 inline size_t hash_value( char* s ) 
 {
     return hash_range( s, s + strlen( s ) );
 }
-
+    
+/** Hash value specialization for @a const @a char*
+ *  @return the hash value
+ *  @relates hash
+ */
 inline size_t hash_value( const char* s ) 
 {
     return hash_range( s, s + strlen( s ) );
 }
 
+/** Hash value specialization for @a std::string
+ *  @return the hash value
+ *  @relates hash
+ */
 inline size_t hash_value( const std::string& s ) 
 {
     return hash_range( s.begin(), s.end() );
 }
 
+/** Hash value specialization for @a std::pair<T1,T2>
+ *  @return the hash value
+ *  @relates hash
+ */
 template <class T1, class T2>
 inline size_t hash_value( const std::pair<T1,T2>& p )
 {
-    size_t seed = 0;
-    hash_combine( seed, p.first );
+    size_t seed = hash_value( p.first );
     hash_combine( seed, p.second );
     return seed;
 }
 
 ///////////////////////////////////////////////////////////////////////
 
-/*! Callable hash function.
+
+/** Combine the hash of various object.
+ *
+ *  @param seed contain the initial seed value and, after the function
+ *  call, will contains the new value.
+ *
+ *  @param v the object to be hashed.
+ *  @relates hash
+ */
+template <class T>
+inline void hash_combine( size_t& seed, const T& v )
+{
+    // Taken from boost::hash::hash_combine
+    // seed ^= hash_value( v ) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+        
+    seed *= 16777619;
+    seed ^= hash_value( v );
+}
+
+///////////////////////////////////////////////////////////////////////
+
+/** Callable hash function.
+ *
+ *  A Hash Function is a Unary Function that is used by Hashed Associative
+ *  Containers: it maps its argument to a result of type size_t. A Hash
+ *  Function must be deterministic and stateless. That is, the return value
+ *  must depend only on the argument, and equal arguments must yield equal
+ *  results.
+ *
+ *  This particular hash implementation, borrow many ideas from boost::hash.
  */
 template <class Key>
 struct hash
     : std::unary_function<Key, size_t>
 {
+    /** Callable operator
+     *
+     *  @param key the object to be hashed.
+     * 
+     *  @return the @a hash value for the given key
+     */
     size_t operator() ( const Key& key ) const
     {
         size_t h = hash_value( key );
         return h;
     }
-};
-
-template <class T>
-inline void hash_combine( size_t& seed, const T& v )
-{
-    // Taken from boost::hash::hash_combine
-    seed ^= hash_value( v ) + 0x9e3779b9 + (seed<<6) + (seed>>2);
     
-    // seed *= 16777619;
-    // seed ^= hash_value( v );
-}
+};
 
 } // namespace mm
 
