02f28ce1e1ae49bf2b92f6bcea9e519eee2b4c23
matteo.merli <matteo.merli@09a6797d-2d1c-0410-8366-433173d232cd>
2007-01-14 16:48:09 +0000

    Changed the default hash combining function, using the one copied from boost.

--- trunk/mm/hash_fun.hpp
+++ trunk/mm/hash_fun.hpp
@@ -204,10 +204,7 @@ template <class T>
 inline void hash_combine( size_t& seed, const T& v )
 {
     // Taken from boost::hash::hash_combine
-    // seed ^= hash_value( v ) + 0x9e3779b9 + (seed<<6) + (seed>>2);
-        
-    seed *= 16777619;
-    seed ^= hash_value( v );
+    seed ^= hash_value( v ) + 0x9e3779b9 + (seed<<6) + (seed>>2);
 }
 
 ///////////////////////////////////////////////////////////////////////
